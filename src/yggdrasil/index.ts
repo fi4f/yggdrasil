6e7385f63fdbe5bc6eecc8a3e324cf946097013f
shawn-mcgee <s.mcgeek07@gmail.com>
2025-01-11 20:48:23 -0700

    Update workflows

---
+++ src/yggdrasil/index.ts
@@ -0,0 +1,12 @@
+import Version from "@fi4f/v";
+
+export namespace Yggdrasil {
+  export const VERSION = Version({
+    moniker: "Yggdrasil",
+    major  : 0,
+    minor  : 0,
+    patch  : 1
+  })
+}
+
+export default Yggdrasil;
