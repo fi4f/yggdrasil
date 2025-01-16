41a3ccb05469c0ce5e140a2404f765870904035e
shawn-mcgee <s.mcgeek07@gmail.com>
2025-01-16 15:06:36 -0700

    Cleaning up

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Version from "@fi4f/v";
 
+import yggy     from "./yggy"
 import Tree     from "./yggy/tree";
 import Node     from "./yggy/node";
 import Path     from "./yggy/path";
@@ -7,16 +8,12 @@ import Action   from "./yggy/action";
 import Context  from "./yggy/context";
 import Listener from "./yggy/listener";
 
-export const VERSION = Version({
-  moniker: "yggy",
-  major  : 0,
-  minor  : 0,
-  patch  : 4
-})
+export const VERSION = yggy.VERSION
 
 export default {
   Version,
   VERSION,
+  yggy,
   Tree,
   Node,
   Path,
---
+++ src/yggy/index.ts
@@ -0,0 +1,12 @@
+import Version from "@fi4f/v";
+
+export namespace yggy {
+  export const VERSION = Version({
+    moniker: "@fi4f/yggy",
+    major  : 0,
+    minor  : 0,
+    patch  : 5
+  })
+}
+
+export default yggy;
